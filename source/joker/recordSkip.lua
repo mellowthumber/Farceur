22b9ba09a853b03b662f914f6d61a29024d65822
mellowthumber <samuelgordonlawson@gmail.com>
2025-08-16 20:38:24 -0500

    Removed debug messages (woops.)

--- source/joker/recordSkip.lua
+++ source/joker/recordSkip.lua
@@ -3,20 +3,15 @@ SMODS.Joker {
     config = { extra = { sChips = 0, gChips = 12 }, },
     calculate = function(self, card, context)
         if context.post_trigger then
-            sendDebugMessage("post_trigger #1: sChips = "..card.ability.extra.sChips)
             card.ability.extra.sChips = card.ability.extra.sChips + card.ability.extra.gChips
-            sendDebugMessage("post_trigger #2: sChips = "..card.ability.extra.sChips)
         end
         if context.joker_main then
-            sendDebugMessage("joker_main: sChips = "..card.ability.extra.sChips)
             return {
                 chips = card.ability.extra.sChips
             }
         end
         if context.post_joker then
-            sendDebugMessage("post_joker #1: sChips = "..card.ability.extra.sChips)
             card.ability.extra.sChips = 0
-            sendDebugMessage("post_joker #2: sChips = "..card.ability.extra.sChips)
         end
     end,
     loc_vars = function(self, info_queue, card)
